3213f2627b98b0c97347fd7354ea10417a571fd4
Marchella Putri Sannie <marchellaputrisannie@192.168.1.13>
2022-09-07 17:33:59 +0700

    routes to connect from splashscreen to get started pages

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: GetStartedPage(),
+      // home: SplashPage(),
+      routes: {
+        '/': (context) => SplashPage(),
+        '/get-started': (context) => GetStartedPage(),
+      },
     );
   }
 }
