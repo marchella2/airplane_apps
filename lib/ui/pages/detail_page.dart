fe99787e60731608ce0861f1ef4ed296196a9bb3
Marchella Putri Sannie <marchellaputrisannie@192.168.1.10>
2023-04-27 22:34:29 +0700

    slicing detail page

---
+++ lib/ui/pages/detail_page.dart
@@ -0,0 +1,284 @@
+import 'package:airplane_apps/shared/theme.dart';
+import 'package:airplane_apps/ui/widgets/custom_button.dart';
+import 'package:airplane_apps/ui/widgets/interest_item.dart';
+import 'package:airplane_apps/ui/widgets/photo_item.dart';
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatelessWidget{
+  const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+
+    Widget backgroundImage(){
+      return Container(
+        width: double.infinity,
+        height: 450,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(
+              'assets/images/image_destination1.png'
+            ),
+          ),
+        ),
+      );
+    }
+
+    Widget customShadow(){
+      return Container(
+        width: double.infinity,
+        height: 214,
+        margin: EdgeInsets.only(top: 236),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              kWhiteColor.withOpacity(0),
+              Colors.black.withOpacity(0.95),
+            ],
+          ),
+        ),
+      );
+    }
+
+    Widget content(){
+      return Container(
+        width: double.infinity,
+        margin: EdgeInsets.symmetric(
+          horizontal: defaultMargin,
+        ),
+        child: Column(
+          children: [
+            // Note : emblem
+            Container(
+              width: 108,
+              height: 24,
+              margin: EdgeInsets.only(
+                top: 30,
+              ),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/images/icon_emblem.png',
+                  ),
+                ),
+              ),
+            ),
+
+            // Note : title
+            Container(
+              margin: EdgeInsets.only(top: 256),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Lake Ciliwung',
+                          style: whiteTextStyle.copyWith(
+                            fontSize: 24,
+                            fontWeight: semiBold,
+                          ),
+                        ),
+                        Text(
+                          'Tangerang',
+                          style: whiteTextStyle.copyWith(
+                            fontSize: 16,
+                            fontWeight: light,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 20,
+                        height: 20,
+                        margin: EdgeInsets.only(right: 2),
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                              'assets/images/icon_star.png',
+                            ),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        '4.8',
+                        style: whiteTextStyle.copyWith(
+                          fontWeight: medium,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            // Note : Description
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.only(top: 30),
+              padding: EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 30,
+              ),
+              decoration: BoxDecoration(
+                color: kWhiteColor,
+                borderRadius: BorderRadius.circular(18),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Note : About
+                  Text(
+                    'About',
+                    style: blackTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                    'Berada di jalur jalan provinsi yang menghubungkan Denpasar Singaraja serta letaknya yang dekat dengan Kebun Raya Eka Karya menjadikan tempat Bali.',
+                    style: blackTextStyle.copyWith(
+                      height: 2,
+                    ),
+                  ),
+
+                  // Note : Photos
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Photos',
+                    style: blackTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Row(
+                    children: [
+                     PhotoItem(
+                      imageUrl: 'assets/images/image_photo1.png',
+                     ),
+                     PhotoItem(
+                      imageUrl: 'assets/images/image_photo2.png',
+                     ),
+                     PhotoItem(
+                      imageUrl: 'assets/images/image_photo3.png',
+                     ),
+                    ],
+                  ),
+
+                  // Note : Interests
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Interests',
+                    style: blackTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Row(
+                    children: [
+                      InterestsItem(
+                        text: 'Kids Park',
+                      ),
+                      InterestsItem(
+                        text: 'Honor Bridge',
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      InterestsItem(
+                        text: 'City Museum',
+                      ),
+                      InterestsItem(
+                        text: 'Central Mall',
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            // Note : Price + Book button 
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.symmetric(vertical: 30),
+              child: Row(
+                children: [
+                  // Note : Price
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'IDR 2.500.000',
+                          style: blackTextStyle.copyWith(
+                            fontSize: 18,
+                            fontWeight: medium,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          'per orang',
+                          style: greyTextStyle.copyWith(
+                            fontWeight: light,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  // Note : Book Button
+                  CustomButton(
+                    title: 'Book Now', 
+                    onPressed: (){},
+                    width: 170,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            backgroundImage(),
+            customShadow(),
+            content(),
+          ],
+        ),
+      ),
+    );
+  }
+}
