6fa08e006cc890affbb45f243915196bddb54600
Marchella Putri Sannie <marchellaputrisannie@192.168.1.18>
2023-05-16 23:17:55 +0700

    slicing choose seat page

--- lib/ui/pages/detail_page.dart
+++ lib/ui/pages/detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:airplane_apps/shared/theme.dart';
+import 'package:airplane_apps/ui/pages/choose_seat_page.dart';
 import 'package:airplane_apps/ui/widgets/custom_button.dart';
 import 'package:airplane_apps/ui/widgets/interest_item.dart';
 import 'package:airplane_apps/ui/widgets/photo_item.dart';
@@ -257,7 +258,14 @@ class DetailPage extends StatelessWidget{
                   // Note : Book Button
                   CustomButton(
                     title: 'Book Now', 
-                    onPressed: (){},
+                    onPressed: (){
+                      Navigator.push(
+                        context, 
+                        MaterialPageRoute(
+                          builder: (context) => ChooseSeatPage()
+                        ),
+                      );
+                    },
                     width: 170,
                   ),
                 ],
