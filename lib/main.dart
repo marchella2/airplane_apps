e70bc999e856fb368d68d8d512a1a86005c8f1b6
Marchella Putri Sannie <marchellaputrisannie@192.168.1.21>
2023-05-19 14:53:13 +0700

    create page cubit and add cubit in homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:airplane_apps/cubit/page_cubit.dart';
 import 'package:airplane_apps/ui/pages/bonus_page.dart';
 import 'package:airplane_apps/ui/pages/get_started_page.dart';
 import 'package:airplane_apps/ui/pages/main_page.dart';
 import 'package:airplane_apps/ui/pages/sign_up_page.dart';
 import 'package:airplane_apps/ui/pages/splash_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,16 +14,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // home: SplashPage(),
-      routes: {
-        '/': (context) => SplashPage(),
-        '/get-started': (context) => GetStartedPage(),
-        '/sign-up':(context) => SignUpPage(),
-        '/bonus':(context) => BonusPage(),
-        '/main':(context) => MainPage(),
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => PageCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        // home: SplashPage(),
+        routes: {
+          '/': (context) => SplashPage(),
+          '/get-started': (context) => GetStartedPage(),
+          '/sign-up':(context) => SignUpPage(),
+          '/bonus':(context) => BonusPage(),
+          '/main':(context) => MainPage(),
+        },
+      ),
     );
   }
 }
