62008770a808fe92f7c7ffc0f272e10ebbd7c1a4
Marchella Putri Sannie <marchellaputrisannie@Marchellas-MacBook-Pro.local>
2023-06-06 14:32:34 +0700

    configure firebase (ios + android)

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,16 @@ import 'package:airplane_apps/ui/pages/get_started_page.dart';
 import 'package:airplane_apps/ui/pages/main_page.dart';
 import 'package:airplane_apps/ui/pages/sign_up_page.dart';
 import 'package:airplane_apps/ui/pages/splash_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
