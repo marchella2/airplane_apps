f803052bfadd91b0484f7a129658f3419cbc6948
Marchella Putri Sannie <marchellaputrisannie@Marchellas-MacBook-Pro.local>
2023-06-27 17:45:46 +0700

    splash screen navigation

--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:airplane_apps/ui/pages/get_started_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:airplane_apps/shared/theme.dart';
 
@@ -16,7 +17,15 @@ class _SplashPageState extends State<SplashPage> {
   void initState() {
     // TODO: implement initState
     Timer(Duration(seconds: 3), (){
-      Navigator.pushNamed(context, '/get-started');
+
+      User? user = FirebaseAuth.instance.currentUser;
+
+      if (user == null){
+        Navigator.pushNamedAndRemoveUntil(context, '/get-started', (route) => false);
+      } else {
+        print(user.email);
+        Navigator.pushNamedAndRemoveUntil(context, '/main', (route) => false);
+      }
     });
     super.initState();
   }
